1db35da1ba3d4605e903f1cf5160cd6d683d7831
Liam Clarke-Hutchinson <liam@steelsky.co.nz>
2022-05-20 20:27:30 +1200

    Fix wrong field in test failure message

--- testingutil/test-helpers.go
+++ testingutil/test-helpers.go
@@ -15,7 +15,7 @@ func ExecuteTests[T any, U any](t *testing.T, testName string, funcUnderTest fun
 	for _, tt := range testCases {
 		t.Run(fmt.Sprintf("%s %v", testName, tt.Input), func(t *testing.T) {
 			if got := funcUnderTest(tt.Input); !reflect.DeepEqual(got, tt.Expected) {
-				t.Errorf("Expected %v, actual was %v", tt.Expected, tt.Input)
+				t.Errorf("Expected %v, actual was %v", tt.Expected, got)
 			}
 		})
 	}
